cef283b57a1b0895f3f34f1133cfc0ed3ef2ba35
Isaac Higgins <zakh508@gmail.com>
2022-12-19 12:31:09 -0500

    Web workers attempt 2

--- docs/js/fractal.js
+++ docs/js/fractal.js
@@ -1,28 +1,72 @@
-// create a new worker
-var worker = new Worker('js/worker.js');
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+var width = canvas.width;
+var height = canvas.height;
+var imageData = ctx.createImageData(width, height);
+var data = imageData.data;
 
-// listen for messages from the worker
-worker.addEventListener('message', function(e) {
-  // draw the image
-  drawImage(e.data);
-});
+var workers = [];
+var workerCount = 4;
+var workerData = [];
+var workerIndex = 0;
 
-// send the worker a message
-worker.postMessage({
-  width: 500,
-  height: 500,
-  x: -0.5,
-  y: 0,
-  scale: 1
-});
+var maxIterations = 100;
+var zoom = 1;
+var panX = 0;
+var panY = 0;
 
-// draw the image
-function drawImage(data) {
-  var canvas = document.getElementById('canvas');
-  var ctx = canvas.getContext('2d');
-  var imageData = ctx.createImageData(data.width, data.height);
-  for (var i = 0; i < data.pixels.length; i++) {
-    imageData.data[i] = data.pixels[i];
+var startTime = Date.now();
+
+for (var i = 0; i < workerCount; i++) {
+  var worker = new Worker('worker.js');
+  worker.onmessage = function(e) {
+    var workerIndex = e.data.workerIndex;
+    var partialData = e.data.data;
+    workerData[workerIndex] = partialData;
+    if (workerData.length == workerCount) {
+      drawImage();
+    }
+  };
+  workers.push(worker);
+}
+
+function drawImage() {
+  for (var i = 0; i < workerData.length; i++) {
+    var partialData = workerData[i];
+    for (var j = 0; j < partialData.length; j++) {
+      data[i * partialData.length + j] = partialData[j];
+    }
   }
   ctx.putImageData(imageData, 0, 0);
+  console.log('Rendered in ' + (Date.now() - startTime) + 'ms');
 }
+
+function render() {
+  workerData = [];
+  for (var i = 0; i < workers.length; i++) {
+    var worker = workers[i];
+    var xmin = -2.5;
+    var xmax = 1;
+    var ymin = -1;
+    var ymax = 1;
+    var x = xmin + (xmax - xmin) * (i / workers.length);
+    var y = ymin + (ymax - ymin) * (i / workers.length);
+    var w = (xmax - xmin) / workers.length;
+    var h = (ymax - ymin) / workers.length;
+    worker.postMessage({
+      workerIndex: i,
+      maxIterations: maxIterations,
+      width: width,
+      height: height,
+      xmin: x,
+      ymin: y,
+      xmax: x + w,
+      ymax: y + h,
+      zoom: zoom,
+      panX: panX,
+      panY: panY
+    });
+  }
+}
+
+render();
--- docs/js/worker.js
+++ docs/js/worker.js
@@ -1,64 +1,38 @@
-// worker.js
-
-// listen for messages from the main script
-self.addEventListener('message', function(e) {
-  // calculate the mandelbrot set
-  var data = calculateMandelbrot(e.data);
-  // send the main script a message
-  self.postMessage(data);
-});
-
-// calculate the mandelbrot set
-function calculateMandelbrot(data) {
-  var width = data.width;
-  var height = data.height;
-  var x = data.x;
-  var y = data.y;
-  var scale = data.scale;
-  var maxIterations = 100;
-  var pixels = new Uint8ClampedArray(width * height * 4);
-  for (var py = 0; py < height; py++) {
-    for (var px = 0; px < width; px++) {
-      var x0 = (px / width) * 3.5 - 2.5;
-      var y0 = (py / height) * 2 - 1;
-      x0 = x0 * scale + x;
-      y0 = y0 * scale + y;
-      var x = 0;
-      var y = 0;
-      var iteration = 0;
-      while (x * x + y * y <= 4 && iteration < maxIterations) {
-        var xtemp = x * x - y * y + x0;
-        y = 2 * x * y + y0;
-        x = xtemp;
-        iteration++;
-      }
-      var p = (py * width + px) * 4;
-      if (iteration < maxIterations) {
-        var c = 3 * Math.log(iteration) / Math.log(maxIterations - 1.0);
-        if (c < 1) {
-          pixels[p + 0] = 255 * c;
-          pixels[p + 1] = 0;
-          pixels[p + 2] = 0;
-        } else if (c < 2) {
-          pixels[p + 0] = 255;
-          pixels[p + 1] = 255 * (c - 1);
-          pixels[p + 2] = 0;
-        } else {
-          pixels[p + 0] = 255;
-          pixels[p + 1] = 255;
-          pixels[p + 2] = 255 * (c - 2);
+onmessage = function(e) {
+  var data = [];
+  var maxIterations = e.data.maxIterations;
+  var width = e.data.width;
+  var height = e.data.height;
+  var xmin = e.data.xmin;
+  var ymin = e.data.ymin;
+  var xmax = e.data.xmax;
+  var ymax = e.data.ymax;
+  var zoom = e.data.zoom;
+  var panX = e.data.panX;
+  var panY = e.data.panY;
+  for (var y = 0; y < height; y++) {
+    for (var x = 0; x < width; x++) {
+      var a = xmin + (xmax - xmin) * x / width;
+      var b = ymin + (ymax - ymin) * y / height;
+      var ca = a;
+      var cb = b;
+      var n = 0;
+      while (n < maxIterations) {
+        var aa = a * a - b * b;
+        var bb = 2 * a * b;
+        a = aa + ca;
+        b = bb + cb;
+        if (a * a + b * b > 16) {
+          break;
         }
-      } else {
-        pixels[p + 0] = 0;
-        pixels[p + 1] = 0;
-        pixels[p + 2] = 0;
+        n++;
       }
-      pixels[p + 3] = 255;
+      var bright = n == maxIterations ? 0 : 255 * Math.log(n) / Math.log(maxIterations - 1);
+      data.push(bright, bright, bright, 255);
     }
   }
-  return {
-    width: width,
-    height: height,
-    pixels: pixels
-  };
-}
+  postMessage({
+    workerIndex: e.data.workerIndex,
+    data: data
+  });
+};
